4977e502700b8c7597f79d1738261cf2a91a95cb
Humaid <humaid@ssrc.tii.ae>
2021-10-17 10:55:34 +0400

    Pull the QTM data properly and show the offset on the page.
    Also adds a reload bodies button.

--- app.py
+++ app.py
@@ -1,21 +1,50 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
-from qualisys import fetch_labels
+from qualisys import QtmChecker
 
 app = Flask(__name__,
         static_url_path='',
         static_folder='static')
+
 labels = [ ]
+qtm = None
+
 @app.route('/', methods=["POST", "GET"])
 def index():
-    if request.args.get("body") in labels:
+    global qtm
+    if request.args.get("body") in qtm.pos:
         # TODO load bookmark
         return render_template('pos.html')
     else:
-        return render_template('index.html', labels=labels)
+        print(qtm.pos)
+        return render_template('index.html', labels=qtm.pos.keys())
 
+@app.route('/pull.json')
+def pull():
+    body = request.args.get("body")
+    if body not in qtm.pos:
+        return {"error": 1}
+    pos = qtm.pos[body]
+    x = float(request.args.get("x"))
+    y = float(request.args.get("y"))
+    z = float(request.args.get("z"))
+    res = {
+        "offset_x": round(x - pos[0], 2),
+        "offset_y": round(y - pos[1], 2),
+        "offset_z": round(z - pos[2], 2),
+        "actual_x": pos[0],
+        "actual_y": pos[1],
+        "actual_z": pos[2],
+    }
+    print(res)
+    return res
+
+@app.route('/reload')
+def reload():
+    qtm.update_bodies()
+    return redirect("/")
 
 if __name__ == '__main__':
-    labels = fetch_labels()
+    qtm = QtmChecker()
 
     app.run(debug=True, host='0.0.0.0')
--- qualisys.py
+++ qualisys.py
@@ -6,26 +6,59 @@ import sys
 import time
 
 import xml.etree.cElementTree as ET
+from threading import Thread
 
 labels = []
 
-def fetch_labels():
-    #asyncio.ensure_future(setup())
-    #asyncio.get_event_loop().run_until_complete()
-    return asyncio.run(setup())
-
-async def setup():
-    global labels
-    """ Main function """
-    connection = await qtm.connect(host="192.168.0.2", version="1.20", timeout=5)
-    if connection is None:
-        print("Connection failed")
-        return
+class QtmChecker(Thread):
+    def __init__(self):
+        Thread.__init__(self)
     
-    params = await connection.get_parameters(parameters=["6d"])
-    xml = ET.fromstring(params)
-    labels = [label.text for label in xml.iter('Name')]
-    print(labels)
-    return labels
+        self.bodies = []
+        self.pos = {}
+        self.connection = None
+        self._stay_open = True
 
+        self.start()
 
+    def run(self):
+        asyncio.run(self._async_run())
+
+    def close(self):
+        self._stay_open = False
+        self.join()
+
+    async def _async_run(self):
+        await self._connect()
+        while(self._stay_open):
+            await asyncio.sleep(1)
+        await self._close()
+
+    async def update_bodies(self):
+        params = await self.connection.get_parameters(parameters=["6d"])
+        xml = ET.fromstring(params)
+        self.bodies = [label.text for label in xml.iter('Name')]
+
+    async def _connect(self):
+        """ Main function """
+        self.connection = await qtm.connect(host="192.168.0.2", version="1.20", timeout=5)
+        if self.connection is None:
+            print("Connection failed")
+            return
+
+        await self.update_bodies()
+
+        await self.connection.stream_frames(
+            components=['6D'],
+            on_packet=self._on_packet)
+
+    def _on_packet(self, packet):
+        header, bodies = packet.get_6d()
+        if bodies is not None:
+            for rigid in self.bodies:
+                index = self.bodies.index(rigid)
+                body = bodies[index]
+                x = round(body[0][0] / 1000, 3)
+                y = round(body[0][1] / 1000, 3)
+                z = round(body[0][2] / 1000, 3)
+                self.pos[rigid] = [x, y, z]
